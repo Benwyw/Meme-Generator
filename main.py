daff34abf3c097463e522ec15fd2346002e55c4a
Ben <57297093+Benwyw@users.noreply.github.com>
2022-05-15 21:34:34 +0800

    Optimize format

---
+++ main.py
@@ -0,0 +1,38 @@
+from PIL import ImageFont, ImageDraw, Image
+from io import BytesIO
+import requests
+
+def generate_meme(url, txt):
+    """Generate and display meme
+
+    Args:
+        url (str): Image url
+        txt (str): Text to display on the image
+    """
+    
+    image = Image.open(BytesIO(requests.get(url).content))
+    draw = ImageDraw.Draw(image)
+    fontsize = 80
+    ttf = 'https://www.dropbox.com/s/cq2bainz70cpbu4/ArialUnicodeMS.ttf?dl=1'
+    color = 'red' #default
+
+    W, H = image.size
+
+    #blank = Image.new('RGB',(1000, 300))
+    font = ImageFont.truetype(requests.get(ttf, stream=True).raw, fontsize)
+
+    w, h = draw.textsize(txt, font=font)
+
+    print('{} {} {} {}'.format((H-h), (H-h)/10, (H-h)/5, (H-h)/2))
+
+    draw.text(((W-w)/2,(H-h)/50), txt, font=font, fill=color) #up
+    draw.text(((W-w)/2,(H-h)/2), txt, font=font, fill=color) #center
+    draw.text(((W-w)/2,(H-h)-((H-h)/50)), txt, font=font, fill=color) #down
+    #image.save('sample-out.png') #save
+    image.show() #display
+    
+def main():
+    generate_meme("https://i.imgur.com/fYe7MrH.jpg", "測試中文字啊啊啊")
+
+if __name__ == '__main__':
+    main()
